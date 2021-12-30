fb135bcf2f0fc023513d0cda4d82dc0bbd2adaf8
Christopher Plumberg <astrophysicist87@gmail.com>
2021-12-30 14:58:21 -0600

    Create generate_volume_rendering.py

---
+++ generate_volume_rendering.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import h5py as h5
+import sys
+from scipy.interpolate import interpn
+from matplotlib import cm                                                                          
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+import volume_renderer
+
+def main():
+    # Load Datacube
+    f = h5.File(sys.argv[1], 'r')
+    x = np.array(f['x'])
+    y = np.array(f['y'])
+    z = np.array(f['z'])
+    datacube = np.array(f['temperature'])
+    
+    # this is where the image array is produced
+    image = render_volume((x,y,z), datacube, (0.0, 0.25*np.pi))
+
+    # Plot Volume Rendering
+    plt.figure(figsize=(4,4), dpi=500)
+    
+    plt.imshow(image)
+    plt.axis('off')
+    
+    # Save figure
+    plt.savefig('volumerender0.png', dpi=500, bbox_inches='tight', pad_inches = 0)
+        
+    return 0
+    
+
+
+  
+if __name__== "__main__":
+  main()
+
