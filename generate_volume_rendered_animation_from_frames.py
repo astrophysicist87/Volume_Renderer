040d5429f4bf6aeb9b70bd6d6fd12c8963afd94e
Christopher Plumberg <astrophysicist87@gmail.com>
2021-12-31 11:40:38 -0600

    Update generate_volume_rendered_animation_from_frames.py

--- generate_volume_rendered_animation_from_frames.py
+++ generate_volume_rendered_animation_from_frames.py
@@ -52,7 +52,7 @@ def animate(i):
 
     # this is where the image array is produced
     image = volume_renderer.render_volume(points, datacube, (0.0, np.pi/4.0), \
-                                          N=100, transferFunction=linearTransferFunction)
+                                          N=100, transferFunction=gaussianTransferFunction)
         
     # z-axis in image points up by default
     # swap axes to get conventional heavy-ion orientation
