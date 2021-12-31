d9736f166959de2eef615802c36e1dc891138e91
Christopher Plumberg <astrophysicist87@gmail.com>
2021-12-30 20:18:08 -0600

    Create generate_volume_rendered_animation_from_frames.py

---
+++ generate_volume_rendered_animation_from_frames.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import h5py as h5
+import sys
+import volume_renderer
+from scipy.interpolate import interpn
+from matplotlib import cm
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+
+def animate(i):
+    print('Rendering Scene ' + str(i+1) + ' of ' + str(len(sys.argv[1:])), flush=True)
+
+    # Load Datacube
+    f = h5.File(sys.argv[i+1], 'r')
+    x = np.array(f['x'])
+    y = np.array(f['y'])
+    z = np.array(f['z'])
+    datacube = np.array(f['temperature'])
+    points = (x, y, z)
+
+    # this is where the image array is produced
+    image = volume_renderer.render_volume(points, datacube, (0.0, np.pi/6.0), N=180)
+        
+    # z-axis in image points up by default
+    # swap axes to get conventional heavy-ion orientation
+    image = np.swapaxes(image, 0, 1)
+
+    plt.imshow(image)
+    plt.axis('off')
+
+
+
+def main():
+
+    # Plot Volume Rendering
+    fig = plt.figure(figsize=(4,4), dpi=80)
+        
+    # Do Volume Rendering at Different Viewing Angles
+    ani = animation.FuncAnimation(fig, animate, np.arange(len(sys.argv[1:])))
+
+    f = "animation.gif" 
+    ani.save(f, writer='imagemagick', fps=10)
+
+    return 0
+
+  
+if __name__== "__main__":
+  main()
+
+
