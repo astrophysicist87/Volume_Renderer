be4216c98a036edee6da819b84e39fc7e6a94880
Christopher Plumberg <astrophysicist87@gmail.com>
2021-12-31 11:00:50 -0600

    Update animation script with custom transfer functions

--- generate_volume_rendered_animation_from_frames.py
+++ generate_volume_rendered_animation_from_frames.py
@@ -10,6 +10,35 @@ from scipy.interpolate import interpn
 from matplotlib import cm
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 
+chosen_colormap = cm.get_cmap('inferno', 256)
+
+def gaussianTransferFunction(x0, **kwargs):
+    frac         = kwargs.get("frac")         if "frac"         in kwargs else 0.0
+    max_opacity  = kwargs.get("max_opacity")  if "max_opacity"  in kwargs else 0.5
+
+    x = np.clip(x0, frac, 1.0)/(1.0-frac)-frac/(1.0-frac)
+    r,g,b,a = np.transpose(np.array(chosen_colormap(x)), axes=[2,0,1])
+    a = max_opacity*np.exp( -(x - 1.0)**2/0.1 )
+    return r,g,b,a
+
+def quadraticTransferFunction(x0, **kwargs):
+    frac         = kwargs.get("frac")         if "frac"         in kwargs else 0.0
+    max_opacity  = kwargs.get("max_opacity")  if "max_opacity"  in kwargs else 0.5
+
+    x = np.clip(x0, frac, 1.0)/(1.0-frac)-frac/(1.0-frac)
+    r,g,b,a = np.transpose(np.array(chosen_colormap(x)), axes=[2,0,1])
+    a = max_opacity*x**2
+    return r,g,b,a
+
+def linearTransferFunction(x0, **kwargs):
+    frac         = kwargs.get("frac")         if "frac"         in kwargs else 0.0
+    max_opacity  = kwargs.get("max_opacity")  if "max_opacity"  in kwargs else 0.5
+
+    x = np.clip(x0, frac, 1.0)/(1.0-frac)-frac/(1.0-frac)
+    r,g,b,a = np.transpose(np.array(chosen_colormap(x)), axes=[2,0,1])
+    a = max_opacity*x
+    return r,g,b,a
+
 def animate(i):
     print('Rendering Scene ' + str(i+1) + ' of ' + str(len(sys.argv[1:])), flush=True)
 
@@ -22,7 +51,8 @@ def animate(i):
     points = (x, y, z)
 
     # this is where the image array is produced
-    image = volume_renderer.render_volume(points, datacube, (0.0, np.pi/3.0), N=500)
+    image = volume_renderer.render_volume(points, datacube, (0.0, np.pi/4.0), \
+                                          N=180, transferFunction=linearTransferFunction)
         
     # z-axis in image points up by default
     # swap axes to get conventional heavy-ion orientation
