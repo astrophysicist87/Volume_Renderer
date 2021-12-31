b474dc330820a23ec247a23b64fd624d8e855b8f
Christopher Plumberg <astrophysicist87@gmail.com>
2021-12-31 11:10:54 -0600

    Try edge-on view

--- generate_volume_rendered_animation_from_frames.py
+++ generate_volume_rendered_animation_from_frames.py
@@ -51,7 +51,7 @@ def animate(i):
     points = (x, y, z)
 
     # this is where the image array is produced
-    image = volume_renderer.render_volume(points, datacube, (0.0, np.pi/4.0), \
+    image = volume_renderer.render_volume(points, datacube, (0.0, 0.0*np.pi/4.0), \
                                           N=180, transferFunction=linearTransferFunction)
         
     # z-axis in image points up by default
