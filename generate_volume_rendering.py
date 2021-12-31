7712a55ff67ae13fba4b0b292c3bf29ddba4edb2
Christopher Plumberg <astrophysicist87@gmail.com>
2021-12-31 10:45:50 -0600

    Update generate_volume_rendering.py

--- generate_volume_rendering.py
+++ generate_volume_rendering.py
@@ -40,7 +40,7 @@ def main():
     
     # this is where the image array is produced
     image = volume_renderer.render_volume((x,y,z), datacube, (0.0, np.pi/3.0), \
-                                          N=180, transferFunction=gaussianTransferFunction)
+                                          N=180, transferFunction=quadraticTransferFunction)
 
     # Plot Volume Rendering
     plt.figure(figsize=(4,4), dpi=500)
