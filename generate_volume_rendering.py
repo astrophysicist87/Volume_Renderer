d3773d5af030fd2529fe6e39ecb3b4508217bcb8
Christopher Plumberg <astrophysicist87@gmail.com>
2021-12-31 10:27:23 -0600

    Add custom transfer functions

--- generate_volume_rendering.py
+++ generate_volume_rendering.py
@@ -9,6 +9,24 @@ from matplotlib import cm
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 import volume_renderer
 
+def gaussianTransferFunction(x0, **kwargs):
+    frac         = kwargs.get("frac")         if "frac"         in kwargs else 0.0
+    max_opacity  = kwargs.get("max_opacity")  if "max_opacity"  in kwargs else 0.5
+
+    x = np.clip(x0, frac, 1.0)/(1.0-frac)-frac/(1.0-frac)
+    r,g,b,a = np.transpose(np.array(chosen_colormap(x)), axes=[2,0,1])
+    a = max_opacity*np.exp( -(x - 1.0)**2/0.1 )
+    return r,g,b,a
+
+def quadraticTransferFunction(x0, **kwargs):
+    frac         = kwargs.get("frac")         if "frac"         in kwargs else 0.0
+    max_opacity  = kwargs.get("max_opacity")  if "max_opacity"  in kwargs else 0.5
+
+    x = np.clip(x0, frac, 1.0)/(1.0-frac)-frac/(1.0-frac)
+    r,g,b,a = np.transpose(np.array(chosen_colormap(x)), axes=[2,0,1])
+    a = max_opacity*x**2
+    return r,g,b,a
+
 def main():
     # Load Datacube
     f = h5.File(sys.argv[1], 'r')
@@ -19,7 +37,8 @@ def main():
     
     
     # this is where the image array is produced
-    image = volume_renderer.render_volume((x,y,z), datacube, (0.0, np.pi/6.0), N=500)
+    image = volume_renderer.render_volume((x,y,z), datacube, (0.0, np.pi/3.0), \
+                                          N=180, transferFunction=gaussianTransferFunction)
 
     # Plot Volume Rendering
     plt.figure(figsize=(4,4), dpi=500)
