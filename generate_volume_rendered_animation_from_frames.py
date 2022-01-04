61c8dce5e591fd188acfee7e98882f49e126c034
Christopher Plumberg <astrophysicist87@gmail.com>
2022-01-04 13:41:42 -0600

    Check effect of fixing color scale maximum at initial time step

--- generate_volume_rendered_animation_from_frames.py
+++ generate_volume_rendered_animation_from_frames.py
@@ -67,7 +67,8 @@ def animate(i):
 
     # this is where the image array is produced
     image = volume_renderer.render_volume(points, datacube, (0.0, np.pi/4.0), N=250, \
-                                          transferFunction=quadraticTransferFunction)
+                                          transferFunction=quadraticTransferFunction,
+                                          scale_max=maximum)
         
     # z-axis in image points up by default
     # swap axes to get conventional heavy-ion orientation
