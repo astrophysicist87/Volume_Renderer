bb0bfd638da829e987671c012a09ab5134af734d
Christopher Plumberg <astrophysicist87@gmail.com>
2022-01-07 14:55:06 -0600

    Does this make it look right?

--- generate_volume_rendered_animation_from_frames.py
+++ generate_volume_rendered_animation_from_frames.py
@@ -74,8 +74,8 @@ def animate(i):
     TFO = 0.154 # freeze-out temperature in GeV
     image = volume_renderer.render_volume(points, datacube, (0.0, np.pi/4.0), N=100, \
                                           transferFunction=linearTransferFunction,
-                                          scale_max=maximum, cutoff=TFO)
-        
+                                          cutoff=TFO)
+    #    scale_max=maximum, 
     # z-axis in image points up by default
     # swap axes to get conventional heavy-ion orientation
     image = np.swapaxes(image, 0, 1)
