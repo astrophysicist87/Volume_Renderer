720307706e4aca85e849975817ceef6645251284
Christopher Plumberg <astrophysicist87@gmail.com>
2022-01-14 17:52:08 -0600

    Another typo

--- convert_tau_frames_to_tz_frames.py
+++ convert_tau_frames_to_tz_frames.py
@@ -48,7 +48,7 @@ for iFrame, frame in enumerate(data):
         unelapsed_taus = tauRange[iFrame:]
         zpts = np.sqrt(unelapsed_taus**2 - t**2)
         zpts = np.unique(np.concatenate((-zpts[::-1],zpts)))
-        tpts = np.unique(np.concatenate((-unelapsed_taus[::-1],unelapsed_taus)))
+        tpts = np.unique(np.concatenate((unelapsed_taus[::-1],unelapsed_taus)))
         Nz = len(zpts)
         
         output = np.tile(frame,(Nz,1,1))
