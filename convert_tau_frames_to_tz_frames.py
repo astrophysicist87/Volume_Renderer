69a75fcc44293764fbe6bdd05867ccdb0eb54059
Christopher Plumberg <astrophysicist87@gmail.com>
2022-01-15 12:03:05 -0600

    Last version was too slow.  Try this instead...

--- convert_tau_frames_to_tz_frames.py
+++ convert_tau_frames_to_tz_frames.py
@@ -9,28 +9,12 @@ Nx = int(sys.argv[2])
 Ny = int(sys.argv[3])
 data = np.array([np.loadtxt(frame) for frame in sys.argv[4:]]) # tau, x, y, e
 
-#tauFinal = 12.43995 # hard-coded for this event!!!
-tauFinal = 12.5 # hard-coded for this event!!!
-dtau = 0.03585
-tFinal = tauFinal
-dt = dtau
-#zmax = 25.0 # hard-coded for this event!!!
-#dz = dxy
-#Nz = int(np.ceil(2.0*zmax/dz))+1
-#if Nz%2==0:
-#    Nz += 1
-
+dt = dtau = 0.03585
 tauRange = data[:,0,0]
 tRange = np.copy(tauRange)
 
-#print('tRange=',tRange)
-
 # add an extra column to data for t coordinates
-#print('data.shape =', data.shape)
 data = np.concatenate( (np.zeros_like(data[:,:,0])[:,:,np.newaxis], data), axis=2 )
-#print('data.shape =', data.shape)
-
-#exit(1)
 
 # doesn't matter what this is, just declare it
 final = None
@@ -53,46 +37,30 @@ for iFrame, frame in enumerate(data):
         final = np.copy(output)
 
     unelapsed_ts = tRange[iFrame:]
-    #print('unelapsed_ts=',unelapsed_ts)
     zpts = np.sqrt(unelapsed_ts**2 - tau**2)
     zpts = np.concatenate((-zpts[-1:0:-1],zpts))
     tpts = np.concatenate((unelapsed_ts[-1:0:-1],unelapsed_ts))
-    #print('tpts=',tpts)
-    #print('zpts=',zpts)
     Nz = len(zpts)
     
     output = np.tile(frame,(Nz,1,1))
 
     # set 0th column to t coordinate, 1st column to z coordinate
-    for iz, zSlice in enumerate(output):
-        #print('zSlice.shape = ', zSlice.shape,'; tpts[iz] =', tpts[iz], zpts[iz])
-        zSlice[:,0] = np.full_like( zSlice[:,0], tpts[iz] )
-        zSlice[:,1] = np.full_like( zSlice[:,1], zpts[iz] )
+    #for iz, zSlice in enumerate(output):
+    #    zSlice[:,0] = np.full_like( zSlice[:,0], tpts[iz] )
+    #    zSlice[:,1] = np.full_like( zSlice[:,1], zpts[iz] )
+    output[:,:,0] = tpts
+    output[:,:,1] = zpts
     
-    #print('output.shape = ', output.shape)
-
     # re-shape to (Nx,Ny,Nz,5) and set column order to t, x, y, z, e
     output = np.swapaxes(output.reshape((Nz,Ny,Nx,5)), 0, 2)[:,:,:,[0,2,3,1,4]]
-    #print(final.shape, output.shape)
     final = np.dstack((final, output))
-    #print(final.shape)
         
 # reshape and sort
 print('Reshape, sort, and split final array', flush=True)
 final = final.reshape([final.size//5,5])
-#final = final[final[:, 0].argsort()]
 final = final[np.lexsort((final[:,3], final[:,2], final[:,1], final[:,0]))]
 
-#final = np.split(final,np.where(np.diff(final[:,0])!=0)[0].tolist())
 final = np.split(final, (np.where(np.diff(final[:,0])>1e-6)[0]+1).tolist())
-
-#print('Obtained', len(final), 'different timesteps')
-
-#for iTimeslice, timeslice in enumerate(final):
-#    print(iTimeslice, timeslice.shape)
-#    np.savetxt('all_frames/post_collision_frames_vs_t/frame_' \
-#               + str(iTimeslice) + '.dat', timeslice, fmt="%lf")
-
 
 print('Saving results', flush=True)
 for iTimeslice, timeslice in enumerate(final):
