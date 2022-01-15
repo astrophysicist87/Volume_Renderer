cc96ab08472af63a26d37eec64a4dbd2daf04166
Christopher Plumberg <astrophysicist87@gmail.com>
2022-01-15 16:41:59 -0600

    This version appears to work.

--- convert_tau_frames_to_tz_frames.py
+++ convert_tau_frames_to_tz_frames.py
@@ -10,38 +10,31 @@ Ny = int(sys.argv[3])
 
 
 def output_to_text(iFrame, data_in, alldata):
-    #print("Output to text")
     data = data_in.reshape([data_in.size//4,4])
-    #print(data.shape)
     data = np.c_[ data, np.zeros((len(data),3)) ]
-    #print(data.shape)
-    #print(data)
     bigdata = np.swapaxes(np.tile(data,(Nx*Ny,1,1)), 0, 1)
-    #print("bigdata.shape=",bigdata.shape)
-    #print(alldata.shape)
     for iTauslice, tauslice in enumerate(bigdata):
-        #print("slice",iTauslice,":",tauslice[:,4:].shape,alldata[int(data[iTauslice,3],),:,1:].shape)
         tauslice[:,4:] = alldata[int(data[iTauslice,3],),:,1:]        
-    #print("bigdata.shape=",bigdata.shape)
     bigdata = (bigdata.reshape([bigdata.size//7,7]))[:,[1,4,5,2,6]]
-    #print("final bigdata.shape=",bigdata.shape)
     bigdata = bigdata[np.lexsort((bigdata[:,3], bigdata[:,2], bigdata[:,1], bigdata[:,0]))]
     np.savetxt('all_frames/post_collision_frames_vs_t/frame_' \
                + str(iFrame) + '.dat', bigdata, fmt="%lf")
     
-def output_to_hdf5(iFrame, data):
-    
-    
-    
-    data = data.reshape([data.size//5,5])
-    data = data[np.lexsort((data[:,3], data[:,2], data[:,1], data[:,0]))]
+def output_to_hdf5(iFrame, data_in, alldata):
+    data = data_in.reshape([data_in.size//4,4])
+    data = np.c_[ data, np.zeros((len(data),3)) ]
+    bigdata = np.swapaxes(np.tile(data,(Nx*Ny,1,1)), 0, 1)
+    for iTauslice, tauslice in enumerate(bigdata):
+        tauslice[:,4:] = alldata[int(data[iTauslice,3],),:,1:]        
+    bigdata = (bigdata.reshape([bigdata.size//7,7]))[:,[1,4,5,2,6]]
+    bigdata = bigdata[np.lexsort((bigdata[:,3], bigdata[:,2], bigdata[:,1], bigdata[:,0]))]
     outfilename = 'all_frames/post_collision_frames_vs_t/frame_' \
                   + str(iFrame).zfill(4) + '.h5'
     hf = h5.File(outfilename, 'w')
-    datasize = len(data)
+    datasize = len(bigdata)
     Nz = datasize//(Nx*Ny)
-    output = data.reshape(Nx,Ny,Nz,5)
-    hf.create_dataset('t', data = data[0,0])
+    output = bigdata.reshape(Nx,Ny,Nz,5)
+    hf.create_dataset('t', data = bigdata[0,0])
     hf.create_dataset('x', data = output[:,0,0,1])
     hf.create_dataset('y', data = output[0,:,0,2])
     hf.create_dataset('z', data = output[0,0,:,3])
@@ -87,9 +80,9 @@ for iFrame, frame in enumerate(data):
     print("\t - set up", flush=True)
     unelapsed_ts = tRange[iFrame:]
     unelapsed_tinds = np.arange(len(tRange))[iFrame:]
-    print(unelapsed_ts)
-    print(unelapsed_tinds)
-    print(tau)
+    #print(unelapsed_ts)
+    #print(unelapsed_tinds)
+    #print(tau)
     zpts = np.sqrt(unelapsed_ts**2 - tau**2)
     zpts = np.concatenate((-zpts[-1:0:-1],zpts))
     tpts = np.concatenate((unelapsed_ts[-1:0:-1],unelapsed_ts))
@@ -104,7 +97,7 @@ for iFrame, frame in enumerate(data):
 
     print("\t - filling", flush=True)
     # set 0th column to t coordinate, 1st column to z coordinate
-    print(output.shape, tpts.shape, zpts.shape)
+    #print(output.shape, tpts.shape, zpts.shape)
     output[:,0] = tinds
     output[:,1] = tpts
     output[:,2] = zpts
@@ -114,17 +107,17 @@ for iFrame, frame in enumerate(data):
     final = np.vstack((final, output))
     
     print("\t - printing", flush=True)
-    print(final.shape)
+    #print(final.shape)
     elements_to_print = (final[:,0] == iFrame)
     #print("1",elements_to_print.shape)
     #print("2a",final[elements_to_print].shape)
-    output_to_text(iFrame, final[elements_to_print], data)
-    #output_to_hdf5(iFrame, final[elements_to_print])
+    #output_to_text(iFrame, final[elements_to_print], data)
+    output_to_hdf5(iFrame, final[elements_to_print], data)
     #print("2b",final[elements_to_print].shape)
     final = final[np.logical_not(elements_to_print)]
     #final = final.reshape([final.size//(Nx*Ny*5),Nx*Ny,5])
     #print("3",final.shape)
-    print(final.shape)
+    #print(final.shape)
         
 
 
