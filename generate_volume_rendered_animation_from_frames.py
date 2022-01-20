35da2de72aac3ddfb599702f0fc520ace8a3a06d
Christopher Plumberg <astrophysicist87@gmail.com>
2022-01-20 16:28:43 -0600

    Update generate_volume_rendered_animation_from_frames.py

--- generate_volume_rendered_animation_from_frames.py
+++ generate_volume_rendered_animation_from_frames.py
@@ -123,7 +123,7 @@ def animate(i):
 def main():
 
     # Plot Volume Rendering
-    fig = plt.figure(figsize=(2,2), dpi=500)
+    fig = plt.figure(figsize=(1,1), dpi=500)
     plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
     plt.margins(0, 0)
         
