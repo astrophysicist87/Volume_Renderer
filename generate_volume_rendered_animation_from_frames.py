61da93067f2570949c9cdd218138901575b93c8d
Christopher Plumberg <astrophysicist87@gmail.com>
2022-01-20 13:54:31 -0600

    Update generate_volume_rendered_animation_from_frames.py

--- generate_volume_rendered_animation_from_frames.py
+++ generate_volume_rendered_animation_from_frames.py
@@ -104,12 +104,12 @@ def animate(i):
     # swap axes to get conventional heavy-ion orientation
     image = np.swapaxes(image, 0, 1)
 
-    #im = plt.imshow(image, cmap=chosen_colormap)
-    ax = plt.gca()
-    im = ax.imshow(image, cmap=chosen_colormap)
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="5%", pad=0.05)
-    plt.colorbar(im, cax=cax)
+    im = plt.imshow(image, cmap=chosen_colormap)
+    #ax = plt.gca()
+    #im = ax.imshow(image, cmap=chosen_colormap)
+    #divider = make_axes_locatable(ax)
+    #cax = divider.append_axes("right", size="5%", pad=0.05)
+    #plt.colorbar(im, cax=cax)
     
     plt.axis('off')
     plt.savefig('animation_frames/frame' + str(i) + '.png', dpi=500, bbox_inches='tight', pad_inches = 0)
