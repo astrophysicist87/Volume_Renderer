b4e32cf832fa4a90c6d649dbac6cf39ec4bb520c
Christopher Plumberg <astrophysicist87@gmail.com>
2022-01-21 15:19:42 -0600

    Update generate_volume_rendered_animation_from_frames.py

--- generate_volume_rendered_animation_from_frames.py
+++ generate_volume_rendered_animation_from_frames.py
@@ -97,7 +97,7 @@ def animate(i):
     eFO = 0.266 # freeze-out temperature in GeV
     TFO = 0.154 # freeze-out temperature in GeV
     image = volume_renderer.render_volume(points, datacube, (0.0, np.pi/2.0), N=image_pixel_dimension, \
-                                          transferFunction=linearTransferFunction, \
+                                          transferFunction=linearTransferFunction, scale_max=maximum,\
                                           max_opacity=1.0, cutoff=eFO, use_log_densities=True)
 
     #image = volume_renderer.render_volume(points, datacube, (0.0, np.pi/2.0), N=image_pixel_dimension, \
