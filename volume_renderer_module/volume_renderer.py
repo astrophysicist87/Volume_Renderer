788a8cf9a08173ed9952550e184537c96ac932f0
Christopher Plumberg <astrophysicist87@gmail.com>
2022-01-21 14:53:24 -0600

    Update volume_renderer.py

--- volume_renderer_module/volume_renderer.py
+++ volume_renderer_module/volume_renderer.py
@@ -72,7 +72,7 @@ def render_volume(points, datacube, angles, **kwargs):
     upsample_factor = 1
     newN = upsample_factor*N+1 if upsample_factor > 1 else N
     #c = np.linspace(-20.0, 20.0, newN)
-    c = np.sort(np.unique(np.concatenate((np.linspace(-20,20,newN),points[0],points[1],points[2]))))
+    c = np.sort(np.unique(np.concatenate((np.linspace(-20,20,newN),np.linspace(-1,1,newN)))))
     print(c,len(c))
     qx, qy, qz = np.meshgrid(c,c,c)
     qxR  = qx
